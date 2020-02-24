71fc11ce33e72d90fdc00ee3be212e71a2a9e56a
Minna Ala-Mantila <minna.ala-mantila@csc.fi>
2020-02-24 16:33:00 +0200

    Added field for ESFRI-classification, style fixes, started keywords en-field.

--- tags.js
+++ tags.js
@@ -93,3 +93,100 @@ function initTags() {
     }
 }
 
+//
+
+// Keywords en
+
+// Intercept document sending with enter
+function interceptEnterEn(evt) {
+    if (evt.keyCode === 13) return false;
+}
+document.onkeypress = interceptEnterEn;
+
+window.addEventListener("DOMContentLoaded", initTagsEn);
+
+function initTagsEn() {
+    const el = document.getElementById('tags-inputEn');
+    let hiddenInputEn = document.createElement('input'),
+        tagsInputEn = document.createElement('input'),
+        tagsEn = [];
+    
+    hiddenInputEn.setAttribute('type', 'hidden');
+    hiddenInputEn.setAttribute('name', el.getAttribute('data-name'));
+
+    tagsInputEn.setAttribute('type', 'text');
+    tagsInputEn.addEventListener('input', function () {
+        let enteredTagsEn = tagsInputEn.value.split(',');
+        if (enteredTagsEn.length > 1) {
+            enteredTagsEn.forEach(function (t) {
+                let filteredTagEn = filterTagEn(t);
+                if (filteredTagEn.length > 0)
+                    addTagEn(filteredTagEn);
+            });
+            tagsInputEn.value = '';
+        }
+    });
+
+    tagsInputEn.addEventListener('keydown', function (e) {
+        let keyCode = e.which || e.keyCode;
+        if (keyCode === 8 && tagsInputEn.value.length === 0) {
+            removeTagEn(tags.lengthEn - 1);
+        }
+
+       if (event.keyCode === 13 && tagsInputEn.value.length > 0 ||
+           event.keyCode ===  9 && tagsInputEn.value.length > 0 ||
+           event.keyCode === 188 && tagsInputEn.value.length > 0 ) {
+            addTagEn(tagsInputEn.value);
+            tagsInputEn.value ="";
+        }
+
+
+    });
+
+    el.appendChild(tagsInputEn);
+    el.appendChild(hiddenInputEn);
+    
+
+    function addTagEn (text) {
+        let tagEn = {
+            text: text,
+            element: document.createElement('span'),
+        };
+
+        tagEn.element.classList.add('tagEn');
+        tagEn.element.textContent = tag.text;
+
+        let closeBtn = document.createElement('span');
+        closeBtn.classList.add('close');
+        tagEn.element.addEventListener('click', function () {
+            removeTagEn(tagsEn.indexOf(tagEn));
+        });
+        tagEn.element.appendChild(closeBtn);
+
+        tagsEn.push(tagEn);
+
+        el.insertBefore(tagEn.element, tagsInputEn);
+
+        refreshTags();
+    }
+
+    function removeTagEn (index) {
+        let tagEn = tagsEn[index];
+        tagsEn.splice(index, 1);
+        el.removeChild(tagEn.element);
+        refreshTagsEn();
+    }
+
+    function refreshTagsEn () {
+        let tagsEnList = [];
+        tagsEn.forEach(function (t) {
+            tagsListEn.push(t.text);
+        });
+        hiddenInput.value = tagsListEn.join(',');
+    }
+
+    function filterTagEn (tagEn) {
+        return tagEn.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+    }
+}
+
