f4d532d024c1ae82eabda489f90b0ff73ae44de7
Juan Fuentes <juan.fuentes@csc.fi>
2021-07-08 12:36:33 +0300

    Added a function 'checkIfSame' that checks if there is a need to toggle the 3b field in the infra form

--- pid.js
+++ pid.js
@@ -20,28 +20,28 @@ function loadDoc() {
 
 function setValues(data) {
     //Ifrastructure name
-    document.getElementById('infraNameFi').value = (data.nameFi == 0) ? "" : data.nameFi
-    document.getElementById('infraNameEn').value = (data.nameEn == 0) ? "" : data.nameEn
-    document.getElementById('infraNameSv').value = (data.nameSv == 0) ? "" : data.nameSv
+    document.getElementById('infraNameFi').value = (data.nameFi == 0) ? "" : data.nameFi;
+    document.getElementById('infraNameEn').value = (data.nameEn == 0) ? "" : data.nameEn;
+    document.getElementById('infraNameSv').value = (data.nameSv == 0) ? "" : data.nameSv;
     //Abbreviation
-    document.getElementById('infraAbbrev').value = (data.acronym == 0) ? "" : data.acronym
+    document.getElementById('infraAbbrev').value = (data.acronym == 0) ? "" : data.acronym;
     //Description
-    document.getElementById('infraDescrFi').value = (data.descriptionFi == 0) ? "" : data.descriptionFi
-    document.getElementById('infraDescrEn').value = (data.descriptionEn == 0) ? "" : data.descriptionEn
-    document.getElementById('infraDescrSv').value = (data.descriptionSv == 0) ? "" : data.descriptionSv
+    document.getElementById('infraDescrFi').value = (data.descriptionFi == 0) ? "" : data.descriptionFi;
+    document.getElementById('infraDescrEn').value = (data.descriptionEn == 0) ? "" : data.descriptionEn;
+    document.getElementById('infraDescrSv').value = (data.descriptionSv == 0) ? "" : data.descriptionSv;
     //Scientific Description
-    document.getElementById('infraDescrSciFi').value = (data.scientificDescriptionFi == 0) ? "" : data.scientificDescriptionFi
-    document.getElementById('infraDescrSciEn').value = (data.scientificDescriptionEn == 0) ? "" : data.scientificDescriptionEn
-    document.getElementById('infraDescrSciSv').value = (data.scientificDescriptionSv == 0) ? "" : data.scientificDescriptionSv
+    document.getElementById('infraDescrSciFi').value = (data.scientificDescriptionFi == 0) ? "" : data.scientificDescriptionFi;
+    document.getElementById('infraDescrSciEn').value = (data.scientificDescriptionEn == 0) ? "" : data.scientificDescriptionEn;
+    document.getElementById('infraDescrSciSv').value = (data.scientificDescriptionSv == 0) ? "" : data.scientificDescriptionSv;
     // Dates
-    document.getElementById('startYear').value = (data.startYear == 0) ? "" : data.startYear
-    document.getElementById('endYear').value = (data.endYear == 0) ? "" : data.endYear
+    document.getElementById('startYear').value = (data.startYear == 0) ? "" : data.startYear;
+    document.getElementById('endYear').value = (data.endYear == 0) ? "" : data.endYear;
 
     //Roadmap, ESFRI
     if (data.finlandRoadmap == true) {
-        document.getElementById("infraAcafi").checked = true
+        document.getElementById("infraAcafi").checked = true;
     }
-    document.getElementById('infraEsfri').value = (data.ESFRICodes == null) ? "" : data.ESFRICodes[0].ESFRICode
+    document.getElementById('infraEsfri').value = (data.ESFRICodes == null) ? "" : data.ESFRICodes[0].ESFRICode;
 
     //Keywords
  
@@ -89,49 +89,51 @@ function setValues(data) {
                     break
                 
             }
-        })
+        });
     }
 
     //2a Service
-    document.getElementById('infraConNameFi').value = (data.infraConPoint[0].infraConNameFi == 0) ? "" : data.infraConPoint[0].infraConNameFi
-    document.getElementById('infraConNameEn').value = (data.infraConPoint[0].infraConNameEn == 0) ? "" : data.infraConPoint[0].infraConNameEn
-    document.getElementById('infraConNameSv').value = (data.infraConPoint[0].infraConNameSv == 0) ? "" : data.infraConPoint[0].infraConNameSv
-    document.getElementById('infraConDescrFi').value = (data.infraConPoint[0].infraConDescrFi == 0) ? "" : data.infraConPoint[0].infraConDescrFi
-    document.getElementById('infraConDescrEn').value = (data.infraConPoint[0].infraConDescrEn == 0) ? "" : data.infraConPoint[0].infraConDescrEn
-    document.getElementById('infraConDescrSv').value = (data.infraConPoint[0].infraConDescrSv == 0) ? "" : data.infraConPoint[0].infraConDescrSv
-    document.getElementById('infraConTel').value = (data.infraConPoint[0].infraConPhone == 0) ? "" : data.infraConPoint[0].infraConPhone
-    document.getElementById('infraConEmail').value = (data.infraConPoint[0].infraConEmail == 0) ? "" : data.infraConPoint[0].infraConEmail
-    document.getElementById('infraConInfoFi').value = (data.infraConPoint[0].infraConInfoFi== 0) ? "" : data.infraConPoint[0].infraConInfoFi
-    document.getElementById('infraConPost').value = (data.infraConPoint[0].infraConPost== 0) ? "" : data.infraConPoint[0].infraConPost
-    document.getElementById('infraConInfoEn').value = (data.infraConPoint[0].infraConInfoEn == 0) ? "" : data.infraConPoint[0].infraConInfoEn
-    document.getElementById('infraConInfoSv').value = (data.infraConPoint[0].infraConInfoSv == 0) ? "" : data.infraConPoint[0].infraConInfoSv
-    document.getElementById('infraConTermsEn').value = (data.infraConPoint[0].infraConTermsEn == 0) ? "" : data.infraConPoint[0].infraConTermsEn
-    document.getElementById('infraConTermsFi').value = (data.infraConPoint[0].infraConTermsFi == 0) ? "" : data.infraConPoint[0].infraConTermsFi
-    document.getElementById('infraConTermsSv').value = (data.infraConPoint[0].infraConTermsSv == 0) ? "" : data.infraConPoint[0].infraConTermsSv
+    document.getElementById('infraConNameFi').value = (data.infraConPoint[0].infraConNameFi == 0) ? "" : data.infraConPoint[0].infraConNameFi;
+    document.getElementById('infraConNameEn').value = (data.infraConPoint[0].infraConNameEn == 0) ? "" : data.infraConPoint[0].infraConNameEn;
+    document.getElementById('infraConNameSv').value = (data.infraConPoint[0].infraConNameSv == 0) ? "" : data.infraConPoint[0].infraConNameSv;
+    document.getElementById('infraConDescrFi').value = (data.infraConPoint[0].infraConDescrFi == 0) ? "" : data.infraConPoint[0].infraConDescrFi;
+    document.getElementById('infraConDescrEn').value = (data.infraConPoint[0].infraConDescrEn == 0) ? "" : data.infraConPoint[0].infraConDescrEn;
+    document.getElementById('infraConDescrSv').value = (data.infraConPoint[0].infraConDescrSv == 0) ? "" : data.infraConPoint[0].infraConDescrSv;
+    document.getElementById('infraConTel').value = (data.infraConPoint[0].infraConPhone == 0) ? "" : data.infraConPoint[0].infraConPhone;
+    document.getElementById('infraConEmail').value = (data.infraConPoint[0].infraConEmail == 0) ? "" : data.infraConPoint[0].infraConEmail;
+    document.getElementById('infraConInfoFi').value = (data.infraConPoint[0].infraConInfoFi== 0) ? "" : data.infraConPoint[0].infraConInfoFi;
+    document.getElementById('infraConPost').value = (data.infraConPoint[0].infraConPost== 0) ? "" : data.infraConPoint[0].infraConPost;
+    document.getElementById('infraConInfoEn').value = (data.infraConPoint[0].infraConInfoEn == 0) ? "" : data.infraConPoint[0].infraConInfoEn;
+    document.getElementById('infraConInfoSv').value = (data.infraConPoint[0].infraConInfoSv == 0) ? "" : data.infraConPoint[0].infraConInfoSv;
+    document.getElementById('infraConTermsEn').value = (data.infraConPoint[0].infraConTermsEn == 0) ? "" : data.infraConPoint[0].infraConTermsEn;
+    document.getElementById('infraConTermsFi').value = (data.infraConPoint[0].infraConTermsFi == 0) ? "" : data.infraConPoint[0].infraConTermsFi;
+    document.getElementById('infraConTermsSv').value = (data.infraConPoint[0].infraConTermsSv == 0) ? "" : data.infraConPoint[0].infraConTermsSv;
 
-
+    var list_2a = Object.values(data.infraConPoint[0]);
     //Organisation name
     const organisations = data.responsibleOrganization[0]
-    document.getElementById('CoOrgNameFi').value = (organisations.responsibleOrganizationNameFi == 0) ? "" : organisations.responsibleOrganizationNameFi
-    document.getElementById('CoOrgNameEn').value = (organisations.responsibleOrganizationNameEn == 0) ? "" : organisations.responsibleOrganizationNameEn
-    document.getElementById('CoOrgNameSv').value = (organisations.responsibleOrganizationNameSv == 0) ? "" : organisations.responsibleOrganizationNameSv
+    document.getElementById('CoOrgNameFi').value = (organisations.responsibleOrganizationNameFi == 0) ? "" : organisations.responsibleOrganizationNameFi;
+    document.getElementById('CoOrgNameEn').value = (organisations.responsibleOrganizationNameEn == 0) ? "" : organisations.responsibleOrganizationNameEn;
+    document.getElementById('CoOrgNameSv').value = (organisations.responsibleOrganizationNameSv == 0) ? "" : organisations.responsibleOrganizationNameSv;
     
     //3a
     data.services.forEach((service,index) => {
         if (index == 0) {
-            document.getElementById('serNameFi').value = (service.serviceNameFi == 0) ? "" : service.serviceNameFi
-            document.getElementById('serNameSv').value = (service.serviceNameSv == 0) ? "" : service.serviceNameSv
-            document.getElementById('serNameEn').value = (service.serviceNameEn == 0) ? "" : service.serviceNameEn
-            document.getElementById('serDescrFi').value = (service.serviceDescriptionFi == 0) ? "" : service.serviceDescriptionFi
-            document.getElementById('serDescrSv').value = (service.serviceDescriptionSv == 0) ? "" : service.serviceDescriptionSv
-            document.getElementById('serDescrEn').value = (service.serviceDescriptionEn == 0) ? "" : service.serviceDescriptionEn
-            document.getElementById('serDescrsciFi').value = (service.serviceScientificDescriptionFi == 0) ? "" : service.serviceScientificDescriptionFi
-            document.getElementById('serDescrsciSv').value = (service.serviceScientificDescriptionSv == 0) ? "" : service.serviceScientificDescriptionSv 
-            document.getElementById('serDescrsciEn').value = (service.serviceScientificDescriptionEn == 0) ? "" : service.serviceScientificDescriptionEn
-            document.getElementById('serAbbrev').value = (service.serviceAcronym == 0) ? "" : service.serviceAcronym
-            document.getElementById('serType').value = (service.serviceType == 0) ? "" : service.serviceType
-            addServices(service,index)
-            addOrg(data,index)
+            document.getElementById('serNameFi').value = (service.serviceNameFi == 0) ? "" : service.serviceNameFi;
+            document.getElementById('serNameSv').value = (service.serviceNameSv == 0) ? "" : service.serviceNameSv;
+            document.getElementById('serNameEn').value = (service.serviceNameEn == 0) ? "" : service.serviceNameEn;
+            document.getElementById('serDescrFi').value = (service.serviceDescriptionFi == 0) ? "" : service.serviceDescriptionFi;
+            document.getElementById('serDescrSv').value = (service.serviceDescriptionSv == 0) ? "" : service.serviceDescriptionSv;
+            document.getElementById('serDescrEn').value = (service.serviceDescriptionEn == 0) ? "" : service.serviceDescriptionEn;
+            document.getElementById('serDescrsciFi').value = (service.serviceScientificDescriptionFi == 0) ? "" : service.serviceScientificDescriptionFi;
+            document.getElementById('serDescrsciSv').value = (service.serviceScientificDescriptionSv == 0) ? "" : service.serviceScientificDescriptionSv;
+            document.getElementById('serDescrsciEn').value = (service.serviceScientificDescriptionEn == 0) ? "" : service.serviceScientificDescriptionEn;
+            document.getElementById('serAbbrev').value = (service.serviceAcronym == 0) ? "" : service.serviceAcronym;
+            document.getElementById('serType').value = (service.serviceType == 0) ? "" : service.serviceType;
+            addServices(service,index);
+            addOrg(data,index);
+            checkIfSame(service,list_2a,index)
+         
             
         }
         else{
@@ -149,6 +151,7 @@ function setValues(data) {
             document.getElementById('serType'+(index-1)).value = (service.serviceType == 0) ? "" : service.serviceType
             addServices(service,index)
             addOrg(data,index)
+            checkIfSame(service,list_2a,index)
         }
     })
     
@@ -192,4 +195,23 @@ function addOrg(data,index) {
         document.getElementById('CoOrgOtherNameSv' + ((index > 0) ? (index-1):"")).value = (org.participantOrganizationNameSv == 0) ? "" : org.participantOrganizationNameSv
 
     }
+}
+
+function checkIfSame(service,list_2a,i) {
+    delete service.servicePoints[0].servicePointInternationalInfraUrl;
+            var list_serPoints = service.servicePoints[0];
+            var is_same = true;
+            Object.values(list_serPoints).every((value,index) => {
+                if (value != list_2a[index]){
+                    is_same = false;
+                    return false;
+                }
+                console.log(is_same);
+                return true;       
+            });
+            if(is_same){
+                document.getElementById('SerPointSame'+ ((i>0) ? (i-1):"")).checked = false;
+                toggleElement('hidden_fields2'+ (((i>0) ? (i-1):"")),false);
+            };
+         
 }
